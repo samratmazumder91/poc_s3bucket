a0194b100601e5ee31795968fb886d5745773878
Samrat Mazumder <samrat.mazumder@softwaysolutions.com>
2017-02-28 15:25:05 +0530

    initial push

---
+++ config/aws/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+return array(
+  'includes' => array('_aws'),
+  'services' => array(
+    'default_settings' => array(
+      'params' => array(
+        'credentials' => array(
+          'key'    => 'AKIAJ5JD7X4UX7LJ3IYA',
+          'secret' => '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',
+        ),
+        'region' => 'us-east-1'
+      )
+    )
+  )
+);
---
+++ index.php
@@ -0,0 +1,203 @@
+<?php namespace SS3;
+
+require 'vendor/autoload.php';
+error_reporting(-1);
+ini_set('display_errors', 'On');
+use Aws\S3\S3Client;
+use Aws\S3\Transfer;
+
+Class PocSThree {
+
+	public function __construct($options = array()){
+		$this->options = $options;
+		$this->S3Client = new S3Client($options);
+	}
+
+	/**
+	 * lists the buckets
+	 */
+	public function listS3Buckets(){
+		return $this->S3Client->listBuckets();
+	}
+
+	/**
+	 * list contents of a bucket
+	 */
+	public function getBucketObjects($bucket_name = ''){
+		if(empty($bucket_name) && empty($this->options['bucket_name'])) {
+			return false;
+		}
+
+		$bucket_name = !empty($bucket_name)? $bucket_name : $this->options['bucket_name'];
+
+		if(empty($bucket_name)) {
+			return false;
+		}
+
+		$objects = array();
+
+		$iterator = $this->S3Client->getIterator('ListObjects',
+			array(
+				'Bucket' => $bucket_name
+			)
+		);
+
+		foreach ($iterator as $object) {
+			$objects[] = $object['Key'];
+		}
+
+		return $objects;
+	}
+
+	/**
+	 * to copy a folder from local to s3
+	 	$source = local directory absolute path
+	 	$destination = s3 bucket
+	 */
+	public function pushDirectoryToBucket($source, $destination){
+		$manager = new Transfer($this->S3Client, $source, $destination);
+		$manager->transfer();
+	}
+
+	/**
+	 * to copy from one bucket to another
+	 	$source = the source bucket name
+	 	$skey = name of the file to copy
+	 	$destination = the destination bucket name
+	 	$dkey = name of the file to be copied (can have a name of your choice)
+	 */
+	public function copyBucketToBucket($source, $skey, $destination, $dkey){
+
+		return $this->S3Client->copyObject(array(
+			'Bucket' => $destination,
+			'Key' => $dkey,
+			'CopySource' => "{$source}/{$skey}"
+		));
+	}
+
+	/**
+	 * to delete a bucket
+	 */
+	public function deleteBucket($bucket_name){
+		return $this->S3Client->deleteBucket(array(
+			'Bucket' => $bucket_name
+		));
+	}
+
+	/**
+	 * 
+	 */
+	public function createBucket($bucket_name, $acl = ''){
+		if($acl == ''){
+			$acl = 'public-read';
+		}
+
+		return $this->S3Client->createBucket(array(
+			'ACL' => $acl,
+			'Bucket' => $bucket_name
+		));
+	}
+
+	/**
+	 * to delete object from bucket
+	 	$bucket_name = bucket name
+	 	$key = the name of the object to be deleted
+	 */
+	public function deleteObject($bucket_name, $key){
+		return $this->S3Client->deleteObject(array(
+			'Bucket' => $bucket_name,
+			'Key' => $key
+		));
+	}
+
+	/**
+	 * to delete multiple objects from bucket
+	 	$bucket_name = bucket name
+	 	$objects = array containing the name of the objects to be deleted
+	 */
+	public function deleteObjects($bucket_name, $objects = array()){
+
+		if($bucket_name == '' || empty($objects)){
+			return false;
+		}
+
+		//create the required array
+		$toDelete = array();
+		foreach ($objects as $value) {
+			$toDelete['Objects'][] = array('Key' => $value);			
+		}
+		return $this->S3Client->deleteObjects(array(
+			'Bucket' => $bucket_name,
+			'Delete' => $toDelete
+		));
+	}
+
+	/**
+	 * to download image from remote url to s3 bucket
+	 	$bucket_name = destination bucket name
+	 	$source_url = url from which image/file is to be downloaded
+	 	$key = destination file name
+	 */
+	public function putInBucketFromUrl($bucket_name, $source_url, $key){
+
+		$WWW_ROOT = '/var/www/flat_public/app/webroot/';
+		$local_path = $WWW_ROOT.'tmp_folder/'.$key;
+		$local_img = file_put_contents($local_path, file_get_contents($source_url));
+
+		if($local_img){
+			return $this->S3Client->putObject(array(
+				'Bucket' => $bucket_name,
+				'SourceFile' => $local_path,
+				'Key' => $key
+			));
+		}
+
+		unlink($local_path);
+	}
+
+	/**
+	 * to create folder inside a bucket
+	 	$bucket_name = destination bucket name
+	 	$folder_name = desired folder name
+	 */
+	public function createFolder($bucket_name, $folder_name){
+		return $this->S3Client->putObject(array(
+				'Bucket' => $bucket_name,
+				'Body' => "",
+				'Key' => $folder_name.'/'
+			));
+	}
+
+	/**
+	 * to upload file to Bucket or folder inside bucket
+	 	$bucket_name = destination bucket name
+	 	$source_file = absolute path of the file
+	 	$key = destination file name
+
+	 	INCASE you want to store the file inside a folder in the bucket,
+	 	$key = folder(s)/file_name
+	 	E.g. : test/sam.jpg
+	 */
+	public function uploadFileToBucket($bucket_name, $source_file, $key){
+		return $this->S3Client->putObject(array(
+			'Bucket' => $bucket_name,
+			'SourceFile' => $source_file,
+			'Key' => $key
+		));
+	}
+
+}
+
+$options = array(
+	'version'     => 'latest',
+	'region'      => 'us-east-1',
+	'bucket_name' => '',
+	'credentials' => array(
+		'key' => '',
+		'secret' => ''
+	)
+);
+
+
+$poc = new PocSThree($options);
+
